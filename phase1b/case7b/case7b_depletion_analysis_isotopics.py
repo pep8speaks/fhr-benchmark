61f8589ec05a6549aab13ac9b2559708cbca1b05
gwenchee <gwendolynchee95@gmail.com>
2020-06-24 16:44:16 +0800

    depletion 7b

--- phase1b/case7b/case7b_depletion_analysis_isotopics.py
+++ phase1b/case7b/case7b_depletion_analysis_isotopics.py
@@ -1,5 +1,5 @@
 """
-This python script runs the depletion analysis for case7b of the FHR 
+This python script runs the isotopic depletion analysis for case7b of the FHR 
 benchmark. 
 
 """
@@ -21,12 +21,10 @@ from phase1b_constants import *
 ###############################################################################
 
 case = 'p1b_c7b'
-results = openmc.deplete.ResultsList.from_hdf5("results/depletion_results_100000p.h5")
+results = openmc.deplete.ResultsList.from_hdf5("results/depletion_results_10000p_nobug.h5")
 
 depletion_keff(results,'long',case)
 
-burnups = [0, 1, 30, 70,160]
-depletion_fission_density_c(burnups,case,100000,'long')
-depletion_neutron_flux_d(case,'long',100000)
-depletion_neutron_flux_e(burnups,case,100000,'long')
-depletion_neutron_spectrum_f(burnups,case,100000,'long')
+depletion_actinides(results,case,'long')
+depletion_fp(results,case,'long')
+depletion_extended(results,case,'long')
