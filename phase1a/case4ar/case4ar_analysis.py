43ee3859df8d585129fa278e97511979604bd09b
gwenchee <gwendolynchee95@gmail.com>
2020-11-07 01:19:12 +0800

    4ar

--- phase1a/case4ar/case4ar_analysis.py
+++ phase1a/case4ar/case4ar_analysis.py
@@ -1,8 +1,8 @@
 """
-PHASE 1A CASE 4AR 
-This python script builds analyzes the tallies from openmc statepoint file 
-and uses functions in scripts/openmc_analysis.py to analyze and manipulate 
-the data into what is required for the FHR benchmark. 
+PHASE 1A CASE 4AR
+This python script builds analyzes the tallies from openmc statepoint file
+and uses functions in scripts/openmc_analysis.py to analyze and manipulate
+the data into what is required for the FHR benchmark.
 """
 
 ###############################################################################
@@ -19,17 +19,36 @@ from openmc_analysis import *
 #                                  Run
 ###############################################################################
 case = 'p1a_4ar'
-keff = 0.83771
-keff_unc = 0.00003
+keff = 0.83437  
+keff_unc = 0.00009  
 
-sp = openmc.StatePoint('h5files/3pcm/statepoint.500.h5')
-beta_b(sp,case)
+sp = openmc.StatePoint('h5files/9pcm/statepoint.500.h5')
+
+beta_b(sp, case)
 # doppler
-print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=0.83600,keff_new_unc=0.00003,temp_change=+50))
-# flibe 
-print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=0.83733,keff_new_unc=0.00003,temp_change=+50))
-# graphite 
-print(reactivity_coefficient_b(keff_og=keff,keff_og_unc=keff_unc,keff_new=0.83398,keff_new_unc=0.00003,temp_change=+50))
+print(
+    reactivity_coefficient_b(
+        keff_og=0.83584,
+        keff_og_unc=0.00010,
+        keff_new=0.83250,
+        keff_new_unc=0.00010,
+        temp_change=+100))
+# flibe
+print(
+    reactivity_coefficient_b(
+        keff_og=0.83474,
+        keff_og_unc=0.00010,
+        keff_new=0.83376,
+        keff_new_unc=0.00010,
+        temp_change=+100))
+# graphite
+print(
+    reactivity_coefficient_b(
+        keff_og=0.83798,
+        keff_og_unc=0.00010,
+        keff_new=0.83080,
+        keff_new_unc=0.00009,
+        temp_change=+100))
 fission_density_c(sp,case)
 neutron_flux_d(sp,keff,keff_unc,case)
 neutron_flux_e(sp,keff,case)
