1ff36d4e6e8c13f9e46c6e2a4848b0244802da4b
gwenchee <gwendolynchee95@gmail.com>
2020-11-24 01:38:01 +0800

    4a coeff

--- phase1a/case4a/case4a_analysis.py
+++ phase1a/case4a/case4a_analysis.py
@@ -30,27 +30,27 @@ print(
     reactivity_coefficient_b(
         keff_og=keff,
         keff_og_unc=keff_unc,
-        keff_new=1.09981,
+        keff_new=1.09522,
         keff_new_unc=0.00010,
         temp_change=+50))
 # flibe
 print(
     reactivity_coefficient_b(
-        keff_og= 1.10263,
-        keff_og_unc=0.00010,
-        keff_new=1.10152,
+        keff_og=keff,
+        keff_og_unc=keff_unc,
+        keff_new=1.09673,
         keff_new_unc=0.00010,
-        temp_change=+100))
+        temp_change=+50))
 # graphite
 print(
     reactivity_coefficient_b(
         keff_og=keff,
         keff_og_unc=keff_unc,
-        keff_new=1.09780,
-        keff_new_unc=0.00011,
+        keff_new=1.09375,
+        keff_new_unc=0.00010,
         temp_change=+50))
 
-fission_density_c(sp, case)
-neutron_flux_d(sp, keff, keff_unc, case)
-neutron_flux_e(sp, keff, case)
-neutron_spectrum_f(sp, case, keff, keff_unc)
+#fission_density_c(sp, case)
+#neutron_flux_d(sp, keff, keff_unc, case)
+#neutron_flux_e(sp, keff, case)
+#neutron_spectrum_f(sp, case, keff, keff_unc)
